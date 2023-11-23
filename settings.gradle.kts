2319980df9d2a535e2ab83751ec75e0566723809
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-23 10:33:37 -0300

    23-11-23

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mini_Clip.TikTok_Clone"
+include(":app")
+ 
