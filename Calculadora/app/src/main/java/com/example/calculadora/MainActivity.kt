33bfd0ab0253dfec3257057c83968bba726d7280
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-23 09:17:28 -0300

    23-11-2023

---
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity()
+{
+
+  //  val textView = findViewById(R.id.resShow) as TextView
+
+
+    //lateinit var et_getRes : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //as variaveis que vão ser utilizadas precisam ser definidas aqui
+
+    }
+
+}
