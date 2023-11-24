c34fe56254397fc6172f139a8cec3525d1564f5f
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-24 09:51:43 -0300

    24-11-23

--- CadastroUsuario/app/src/main/java/com/example/cadastrousuario/MainActivity.kt
+++ CadastroUsuario/app/src/main/java/com/example/cadastrousuario/MainActivity.kt
@@ -1,52 +1,33 @@
 package com.example.cadastrousuario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+
+ private lateinit var btnCadastro: Button
+ private lateinit var btnLogin: Button
+
 
 class MainActivity : AppCompatActivity() {
-
-
-   private lateinit var nome: EditText
-   private lateinit var cpf: EditText
-   private lateinit var email: EditText
-   private lateinit var senha: EditText
-   private lateinit var enviar: Button
-   private lateinit var c: Connection
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nome = findViewById(R.id.nome)
-        cpf = findViewById(R.id.cpf)
-        email = findViewById(R.id.email)
-        senha = findViewById(R.id.senha)
-        enviar = findViewById(R.id.enviar)
-        c = Connection(this)
+        btnCadastro = findViewById(R.id.btnCadastro)
+        btnLogin = findViewById(R.id.btnLogin)
 
-        enviar.setOnClickListener{
-            val nometext = nome.text.toString()
-            val cpftext = cpf.text.toString()
-            val emailtext = email.text.toString()
-            val senhatext = senha.text.toString()
-            //armazena estes dados
-            val savedata = c.insertValues(nometext,cpftext,emailtext,senhatext)
-
-            if(TextUtils.isEmpty(nometext) || TextUtils.isEmpty(cpftext) || TextUtils.isEmpty(emailtext) || TextUtils.isEmpty(senhatext) ){
-                Toast.makeText(this, "Preencha todos os campos corretamente!!", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                Toast.makeText(this, "Cadastro feito com sucesso!!", Toast.LENGTH_SHORT).show()
-            }
-
+        btnCadastro.setOnClickListener{
+            val Intent = Intent(this, cadastro_user::class.java)
+            startActivity(Intent)
+        }
+        //btnLogin.setOnClickListener{
+        //    val intent = Intent(this, cada::class.java)
+         //   startActivity(Intent)
+        //}
 
         }
 
 
 
     }
-}
