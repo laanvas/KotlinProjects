fc9a0131261e28e0b883882e89ae5e38b9829e4f
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-24 11:30:04 -0300

    24-11-23

--- CadastroUsuario/app/src/main/java/com/example/cadastrousuario/login_user.kt
+++ CadastroUsuario/app/src/main/java/com/example/cadastrousuario/login_user.kt
@@ -2,8 +2,12 @@ package com.example.cadastrousuario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login_user : AppCompatActivity() {
+
+    private lateinit var login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_user)
