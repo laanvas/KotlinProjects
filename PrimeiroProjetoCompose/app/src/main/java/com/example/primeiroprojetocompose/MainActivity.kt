1005fdf2fed29444c4a878403bfd4dd1c13ec930
Alan Vasconcelos <145019770+laanvas@users.noreply.github.com>
2023-11-25 14:57:55 -0300

    25-11-23

--- PrimeiroProjetoCompose/app/src/main/java/com/example/primeiroprojetocompose/MainActivity.kt
+++ PrimeiroProjetoCompose/app/src/main/java/com/example/primeiroprojetocompose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.primeiroprojetocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.primeiroprojetocompose.ui.theme.PrimeiroProjetoComposeTheme
 
@@ -32,13 +36,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(){
-    Text("Hello World")
+    Box(modifier = Modifier.fillMaxSize().background(Color.Black)){
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PrimeiroProjetoComposeTheme {
-        App()
+        Surface(modifier = Modifier.fillMaxSize()){
+            App()
+        }
     }
 }
